d330889a5cc41128cf4b1e6cdeb56b60d60d5267
Tayyaba Bagwan <166994713+tayyaba-keystone@users.noreply.github.com>
2025-06-19 12:14:58 +0530

    Add files via upload
    host the Dimart web

---
+++ host_application.tf
@@ -0,0 +1,80 @@
+
+# create default vpc if one does not exit
+resource "aws_default_vpc" "default_vpc" {
+
+  tags    = {
+    Name  = "default vpc"
+  }
+}
+
+
+# use data source to get all avalablility zones in region
+data "aws_availability_zones" "available_zones" {}
+
+
+# create default subnet if one does not exit
+resource "aws_default_subnet" "default_az1" {
+  availability_zone = data.aws_availability_zones.available_zones.names[0]
+
+  tags   = {
+    Name = "default subnet"
+  }
+}
+
+
+# create security group for the ec2 instance
+resource "aws_security_group" "ec2_security_group" {
+  name        = "ec2 security group"
+  description = "allow access on ports 80 and 22"
+  vpc_id      = aws_default_vpc.default_vpc.id
+
+  ingress {
+    description      = "http access"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "ssh access"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["152.58.17.89/32"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = -1
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags   = {
+    Name = "ec2 security group"
+  }
+}
+
+
+
+
+# launch the ec2 instance and install website
+resource "aws_instance" "ec2_instance" {
+  ami                    = "ami-032ae1bccc5be78ca"
+  instance_type          = "t2.micro"
+  subnet_id              = aws_default_subnet.default_az1.id
+  vpc_security_group_ids = [aws_security_group.ec2_security_group.id]
+  key_name               = "iac"
+  user_data              = file("user_data.sh")
+
+  tags = {
+    Name = "Dimart Server"
+  }
+}
+
+
+# print the ec2's public ipv4 address
+output "public_ipv4_address" {
+  value = aws_instance.ec2_instance.public_ip
+}
